407220e63c00488ebb9b8f131228e0baa219c7bb
Maximiliano Pisfil <maxi.pi@uc.cl>
2025-04-27 18:27:56 -0400

    Setup

---
+++ functions.c
@@ -0,0 +1,12 @@
+// main.c
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello from CMake project!\n");
+    return 0;
+}
+// gcc functions.c -o functions
+// ./functions
---
+++ main.py
@@ -0,0 +1,6 @@
+
+print("Hello world!")
+for i in range(3):
+    print(f"")
+    for j in range(4):
+        print(f"{i}, {j}")
